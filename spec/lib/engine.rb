28e67a57988e69fe2d858508f98c69875da77219
Sebastian Prestel <sebastian.prestel@yahoo.de>
2013-01-16 21:12:54 +0100

    default_base handles ENV["RAILS_RELATIVE_URL_ROOT"]

---
+++ spec/lib/engine.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module TinyMCE::Rails
+  describe Engine do
+    describe ".default_base" do
+
+      specify {TinyMCE::Rails::Engine.default_base.should eql "/assets/tinymce"}
+
+      it "should include relative_url_root" do
+        Rails.application.config.stub!(:relative_url_root).and_return "/prefix"
+        TinyMCE::Rails::Engine.default_base.should eql "/prefix/assets/tinymce"
+      end
+
+      it "should be only /tinymce if no parameter is given" do
+        Rails.application.config.assets.stub!(:prefix).and_return nil
+        TinyMCE::Rails::Engine.default_base.should eql "/tinymce"
+      end
+    end
+  end
+end
